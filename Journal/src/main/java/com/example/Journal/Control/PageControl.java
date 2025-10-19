0f343968d0254ffaf8bb463e70e3bbac021283ac
Shubh Ramani <ramanishubh@gmail.com>
2025-10-19 19:06:42 +0530

    Refactor import statements in PageControl and UserControl for improved organization

--- Journal/src/main/java/com/example/Journal/Control/PageControl.java
+++ Journal/src/main/java/com/example/Journal/Control/PageControl.java
@@ -1,7 +1,6 @@
 package com.example.Journal.Control;
 
 import org.bson.types.ObjectId;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- Journal/src/main/java/com/example/Journal/Control/UserControl.java
+++ Journal/src/main/java/com/example/Journal/Control/UserControl.java
@@ -1,15 +1,21 @@
 package com.example.Journal.Control;
 
-import com.example.Journal.Entity.User;
-import com.example.Journal.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.Journal.Entity.User;
+import com.example.Journal.Service.UserService;
+
 
 @RestController
 @RequestMapping("/user")
